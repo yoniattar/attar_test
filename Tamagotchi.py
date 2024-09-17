5d34d8c84d7ba46cfe5ca0f5078b27060a8fcdfa
yoniattar <yonatana2007@gmail.com>
2024-09-17 13:22:35 +0300

    base tamagotchi class with functions

--- Tamagotchi.py
+++ Tamagotchi.py
@@ -7,3 +7,19 @@ class Tamagotchi:
         self.sickness = sickness
         self.age = age
         self.weight = weight
+    def GetAge(self):
+        return self.age
+    def GetWeight(self):
+        return self.weight
+    def Eat(self, food):
+        self.hunger -= 1
+        self.weight += 1
+        self.sickness -= 1
+    def MiniGame(self):
+        self.hunger += 1
+        self.happiness += 1
+        self.training += 1
+    def Sleep(self, hours):
+        self.sickness -= hours
+        self.happiness += hours
+
