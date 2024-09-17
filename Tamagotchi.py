f96ef780164d53bad25d4c46efd48cdecaf7b637
yoniattar <yonatana2007@gmail.com>
2024-09-17 13:05:45 +0300

    Base Tamagotchi class

---
+++ Tamagotchi.py
@@ -0,0 +1,9 @@
+class Tamagotchi:
+    def __init__(self, stat, hunger, happiness, training, sickness, age, weight):
+        self.stat = stat
+        self.hunger = hunger
+        self.happiness = happiness
+        self.training = training
+        self.sickness = sickness
+        self.age = age
+        self.weight = weight
