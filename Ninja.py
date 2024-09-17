005321996a9c653e3b6e49d01b63959bc9a5b8c3
yoniattar <yonatana2007@gmail.com>
2024-09-17 14:00:27 +0300

    all classes

---
+++ Ninja.py
@@ -0,0 +1,15 @@
+from Tamagotchi import *
+class Ninja(Tamagotchi):
+    def __init__(self, stat, hunger, happiness, training, sickness, age, weight, weapon, weapon_lvl):
+        Tamagotchi.__init__(stat, hunger-5, happiness-2, training+5, sickness-2, age, weight+10)
+        self.weapon = weapon
+    def Mission(self, lvl):
+        self.hunger += lvl*3
+        self.training += lvl*5
+        self.sickness += lvl*2
+        lst_of_weapons = ['bow', 'crossbow', 'knife', 'shurikans', 'sword', 'hammer', 'scythe', 'spear', 'axe', 'boomerang', 'stick']
+
+
+
+
+
